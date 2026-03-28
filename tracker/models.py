c40c9151aefe033bab6826f86ee3dac3597b4c0b
Muneebtrickey <itsmuneeb566@gmail.com>
2026-03-28 14:46:13 +0500

    add functionality is added

--- tracker/models.py
+++ tracker/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Problem(models.Model):
+    DIFFICULTY_CHOICES = [
+        ('Easy', 'Easy'),
+        ('Medium', 'Medium'),
+        ('Hard', 'Hard'),
+    ]
+
     title = models.CharField(max_length=200)
-    difficulty = models.CharField(max_length=10)
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
     platform = models.CharField(max_length=100, default="LeetCode")
     date_solved = models.DateField(auto_now_add=True)
 
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Problem
 
 def home(request):
     problems = Problem.objects.all().order_by('-date_solved')
     return render(request, 'home.html', {'problems': problems})
+
+
+def add_problem(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        difficulty = request.POST.get('difficulty')
+
+        Problem.objects.create(
+            title=title,
+            difficulty=difficulty
+        )
+
+        return redirect('home')
+
+    return render(request, 'add.html')
